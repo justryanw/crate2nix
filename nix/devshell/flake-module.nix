2e9aa6252d560b7f443fcd77cbbb6bdaa7a0ebf7
Ryan Walker <ryanjwalker2001@gmail.com>
2024-07-03 16:29:08 +0100

    cleanup

--- nix/devshell/flake-module.nix
+++ nix/devshell/flake-module.nix
@@ -10,16 +10,25 @@
       config.devshells.default = {
         imports = [
           "${inputs.devshell}/extra/language/c.nix"
-          # "${inputs.devshell}/extra/language/rust.nix"
         ];
 
         commands = with pkgs; [
           { package = rust-toolchain; category = "rust"; }
+          { package = (writeShellScriptBin "cache" ''
+            echo "Caching inputs"
+            nix flake archive --json $1 | ${jq}/bin/jq -r '.path,(.inputs|to_entries[].value.path)' | cachix push justryanw
+            echo "Caching runtime closure"
+            nix build --json $1 | ${jq}/bin/jq -r '.[].outputs | to_entries[].value' | cachix push justryanw;
+          ''); }
         ];
 
         language.c = {
           libraries = lib.optional pkgs.stdenv.isDarwin pkgs.libiconv;
         };
+
+        env = [
+          { name = "RUST_SRC_PATH"; value = "${pkgs.rustPlatform.rustLibSrc}"; }
+        ];
       };
     };
 }
